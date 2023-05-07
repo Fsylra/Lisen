a993d57025e69f2c27a7ad3757556e6dfe35d448
Fsylra <95554552+Fsylra@users.noreply.github.com>
2023-05-07 01:19:03 -0300

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,75 @@
+let allMusic = [
+    {
+        name: "All My Pride",
+        artist: "Black Honey",
+        img: "music-1",
+        src: "music-1"
+      },
+      {
+        name: "Fall Away",
+        artist: "Twenty One Pilots",
+        img: "self-titled-top",
+        src: "fall-away"
+      },
+      {
+        name: "Call me back",
+        artist: "The Strokes",
+        img: "music-3",
+        src: "call-me-back"
+      },
+      {
+        name: "DiE4u",
+        artist: "Bring Te The Horizon",
+        img: "music-4",
+        src: "die4u"
+      },
+      {
+        name: "Helena",
+        artist: "My Chemical Romance",
+        img: "music-5",
+        src: "Helena"
+      },
+      {
+        name: "Knee Socks",
+        artist: "Arctic Monkeys",
+        img: "music-6",
+        src: "Knee-Socks"
+      },
+      {
+        name: "Selfless",
+        artist: "The Strokes",
+        img: "music-6",
+        src: "selfless"
+      },
+      {
+        name: "Sea",
+        artist: "BTS",
+        img: "music-6",
+        src: "sea"
+      },
+      {
+        name: "No Buses",
+        artist: "The Strokes",
+        img: "music-6",
+        src: "no-buses"
+      },
+      {
+        name: "Never too Late",
+        artist: "Three Days Grace",
+        img: "music-6",
+        src: "never-too-late"
+      },
+      {
+        name: "Johnny Boy",
+        artist: "Twenty One Pilots",
+        img: "self-titled-top",
+        src: "johnny-boy"
+      },
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
